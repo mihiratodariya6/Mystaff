cd633e34a8f66eb0526b59b9606984ebb3392c16
mihiratodariya6 <mihiratodariya6@gmail.com>
2026-04-04 09:52:06 +0000

    Final push after fixing gradle settings

--- mystaff/android/app/build.gradle.kts
+++ mystaff/android/app/build.gradle.kts
@@ -11,8 +11,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        coreLibraryDesugaringEnabled true
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
@@ -41,4 +42,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// 👇 આ મેં નવું ઉમેરી દીધું છે 
+dependencies {
+    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.3'
 }
